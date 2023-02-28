577af2c76e4e57e03908abf3e90de2bc26d42cc6
DmitryLorents <zmey.task@gmail.com>
2023-02-28 17:01:20 +0600

    - background pdf added

--- WheatherApp/SceneDelegate.swift
+++ WheatherApp/SceneDelegate.swift
@@ -10,15 +10,15 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
-    var navController: UINavigationController?
+    //var navController: UINavigationController?
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         
         guard let scene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: scene)
-        navController = UINavigationController(rootViewController: ViewController())
-        window?.rootViewController = navController
+        //navController = UINavigationController(rootViewController: ViewController())
+        window?.rootViewController = ViewController()
         window?.backgroundColor = .systemBackground
         window?.makeKeyAndVisible()
     }
