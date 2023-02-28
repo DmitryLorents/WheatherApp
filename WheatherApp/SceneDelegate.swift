a36d8d73d47d146d19faf5b0e49b99dc2e5ac28a
DmitryLorents <zmey.task@gmail.com>
2023-02-28 08:29:59 +0600

    - navController added

--- WheatherApp/SceneDelegate.swift
+++ WheatherApp/SceneDelegate.swift
@@ -10,13 +10,15 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
+    var navController: UINavigationController?
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         
         guard let scene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: scene)
-        window?.rootViewController = ViewController()
+        navController = UINavigationController(rootViewController: ViewController())
+        window?.rootViewController = navController
         window?.backgroundColor = .systemBackground
         window?.makeKeyAndVisible()
     }
