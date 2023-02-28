d5c424c8c37647825fbd9c3e639b64a55b2414b7
DmitryLorents <zmey.task@gmail.com>
2023-02-28 17:31:03 +0600

    - adjust images settings add image to ViewController

--- WheatherApp/ViewController.swift
+++ WheatherApp/ViewController.swift
@@ -8,12 +8,33 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let backGroundView = UIImageView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        style()
+        layout()
+        
     }
 
 
 }
-
+extension ViewController {
+    func style() {
+        backGroundView.translatesAutoresizingMaskIntoConstraints = false
+        backGroundView.image = UIImage(named: "background")
+        backGroundView.contentMode = .scaleAspectFill
+    }
+    
+    func layout() {
+        view.addSubview(backGroundView)
+        //autoLayout
+        NSLayoutConstraint.activate([
+            backGroundView.topAnchor.constraint(equalTo: view.topAnchor),
+            backGroundView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            backGroundView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            backGroundView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
+    }
+}
