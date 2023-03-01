cdb6a1aeebe1dbf522a40584dbada4dfc757f377
DmitryLorents <zmey.task@gmail.com>
2023-03-01 15:27:53 +0600

    - location button added

--- WheatherApp/ViewController.swift
+++ WheatherApp/ViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
+    let locationButton = UIButton()
     let backGroundView = UIImageView()
 
     override func viewDidLoad() {
@@ -22,19 +22,30 @@ class ViewController: UIViewController {
 }
 extension ViewController {
     func style() {
+        
         backGroundView.translatesAutoresizingMaskIntoConstraints = false
         backGroundView.image = UIImage(named: "background")
         backGroundView.contentMode = .scaleAspectFill
+        
+        locationButton.translatesAutoresizingMaskIntoConstraints = false
+        locationButton.setBackgroundImage(UIImage(systemName: "location.circle.fill"), for: .normal)
+        locationButton.tintColor = .label
     }
     
     func layout() {
         view.addSubview(backGroundView)
+        view.addSubview(locationButton)
         //autoLayout
         NSLayoutConstraint.activate([
             backGroundView.topAnchor.constraint(equalTo: view.topAnchor),
             backGroundView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             backGroundView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
-            backGroundView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+            backGroundView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            
+            locationButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            locationButton.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 8),
+            locationButton.heightAnchor.constraint(equalToConstant: 44),
+            locationButton.widthAnchor.constraint(equalTo: locationButton.heightAnchor)
         ])
     }
 }
