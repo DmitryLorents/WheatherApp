0669cf8b99a66bbaadd4f9ba09ed76b4d8cdfc91
DmitryLorents <zmey.task@gmail.com>
2023-03-01 15:49:37 +0600

    - search button added

--- WheatherApp/ViewController.swift
+++ WheatherApp/ViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class ViewController: UIViewController {
     let locationButton = UIButton()
+    let searchButton = UIButton()
     let backGroundView = UIImageView()
 
     override func viewDidLoad() {
@@ -30,12 +31,20 @@ extension ViewController {
         locationButton.translatesAutoresizingMaskIntoConstraints = false
         locationButton.setBackgroundImage(UIImage(systemName: "location.circle.fill"), for: .normal)
         locationButton.tintColor = .label
+        
+        searchButton.translatesAutoresizingMaskIntoConstraints =  false
+        searchButton.setBackgroundImage(UIImage(systemName: "magnifyingglass"), for: .normal)
+        searchButton.tintColor = .label
     }
     
     func layout() {
+        //add views
         view.addSubview(backGroundView)
         view.addSubview(locationButton)
+        view.addSubview(searchButton)
+        
         //autoLayout
+        
         NSLayoutConstraint.activate([
             backGroundView.topAnchor.constraint(equalTo: view.topAnchor),
             backGroundView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
@@ -43,9 +52,14 @@ extension ViewController {
             backGroundView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             
             locationButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            locationButton.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 8),
+            locationButton.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 1),
             locationButton.heightAnchor.constraint(equalToConstant: 44),
-            locationButton.widthAnchor.constraint(equalTo: locationButton.heightAnchor)
+            locationButton.widthAnchor.constraint(equalTo: locationButton.heightAnchor),
+            
+            searchButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            view.trailingAnchor.constraint(equalToSystemSpacingAfter: searchButton.trailingAnchor, multiplier: 1),
+            searchButton.heightAnchor.constraint(equalTo: locationButton.heightAnchor),
+            searchButton.widthAnchor.constraint(equalTo: searchButton.heightAnchor)
         ])
     }
 }
